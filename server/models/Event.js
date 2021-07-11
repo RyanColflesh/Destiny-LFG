ee8ab9d69d06cc657f5a6755d939a40c9107f2c9
Ryan Colflesh <rcc223@lehigh.edu>
2021-07-10 20:31:37 -0400

    Added event routes to update users

--- server/models/Event.js
+++ server/models/Event.js
@@ -1,12 +1,28 @@
 const mongoose = require('mongoose');
 
+/* 
+Database schema for Event object
+-------------------------------------------------------------------------------------------------
+description: description for event
+tags: categories for event
+submitterID: user ID of the event submitter
+requestedUsers: user IDs of the users who have requested to join the event
+joinedUsers: user IDs of the users whose request to join the event were accepted by the submitter
+date: time the event was posted
+*/
+
 const EventSchema = mongoose.Schema({
     description: {
         type: String,
         required: true
     },
     tags: [String],
-    userID: String,
+    submitterID: {
+        type: String,
+        required: true
+    },
+    requestedUsers: [String],
+    joinedUsers: [String],
     date: Date
 });
 
--- server/routes/events.js
+++ server/routes/events.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Event = require('../models/Event');
 
+//Get all events
 router.get('/', async (req, res) => {
     try {
         const events = await Event.find();
@@ -11,11 +12,15 @@ router.get('/', async (req, res) => {
     }
 });
 
+//Add event to database
 router.post('/', async (req,res) => {
     const event = new Event({
         description: req.body.description,
         tags: req.body.tags,
-        userID: req.body.userID
+        submitterID: req.body.submitterID,
+        requestedUsers: req.body.requestedUsers,
+        joinedUsers: req.body.joinedUsers,
+        date: req.body.date
     });
 
     try {
@@ -25,6 +30,30 @@ router.post('/', async (req,res) => {
         res.json({message: err});
     }
 
+});
+
+//Delete event by ID
+router.delete('/:postID', async (req,res) => {
+    try {
+        const removedEvent = await Event.deleteOne({_id: req.params.postID});
+        res.json(removedEvent);
+    } catch(err) {
+        res.json({message: err});
+    }
+});
+
+//Adds requested user(s) to event by ID
+router.patch('/addRequestedUser/:postID', async(req, res) => {
+    console.log(req.params.postID);
+    try {
+        const updatedEvent = await Event.updateOne(
+            {_id: req.params.postID}, 
+            {$push: {requestedUsers : req.body.requestedUser}}
+        );
+        res.json(updatedEvent);
+    } catch(err) {
+        res.json({message : err});
+    } 
 });
 
 module.exports = router;
